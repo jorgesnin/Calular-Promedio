e6a60ae6a6e9bcdae35a7cb6a5185415aa9f1c94
JORGESN <jorgesn@gmail.com>
2026-02-08 22:07:08 -0300

    actualizacion código calcular promedio

--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
-// Pedir las 3 notas
+// pidenotas
 let nota1 = parseFloat(prompt("Ingresa la nota 1:"));
 let nota2 = parseFloat(prompt("Ingresa la nota 2:"));
 let nota3 = parseFloat(prompt("Ingresa la nota 3:"));
 
-// Calcular promedio
-let promedio = (nota1 + nota2 + nota3) / 3;
-
-// Evaluar y mostrar resultado
-if (promedio >= 6.0) {
-    alert(`¡Excelente! Tienes promedio ${promedio}.`);
-} else if (promedio >= 5.0) {
-    alert(`Tienes promedio ${promedio}. ¡Sigue adelante, puedes mejorar!`);
-} else if (promedio >= 4.0) {
-    alert(`Tienes promedio ${promedio}. Debes esforzarte más.`);
+// comprobar numeros
+if (isNaN(nota1) || isNaN(nota2) || isNaN(nota3)) {
+    document.querySelector("#resultado").innerHTML = "Error: Debes ingresar números válidos.";
 } else {
-    alert(`Has reprobado con promedio ${promedio}. Continúa intentándolo.`);
+    // promedio
+    let promedio = (nota1 + nota2 + nota3) / 3;
+
+    // resultado
+    if (promedio >= 6.0) {
+        document.querySelector("#resultado").innerHTML = `¡Excelente! Tienes promedio ${promedio}.`;
+    } else if (promedio >= 5.0) {
+        document.querySelector("#resultado").innerHTML = `Tienes promedio ${promedio}. ¡Sigue adelante, puedes mejorar!`;
+    } else if (promedio >= 4.0) {
+        document.querySelector("#resultado").innerHTML = `Tienes promedio ${promedio}. Debes esforzarte más.`;
+    } else {
+        document.querySelector("#resultado").innerHTML = `Has reprobado con promedio ${promedio}. Continúa intentándolo.`;
+    }
 }
